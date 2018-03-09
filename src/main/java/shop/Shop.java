7a67bcbf5dd4a6289b5e1058c08f2d3fe1003b30
Eleftheria Koronidi <ria@koronidi.com>
2018-03-09 16:44:25 +0000

    Added methods to add and remove items in stock and tested these

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,6 +1,7 @@
 package shop;
 
 import accessories.DrumStick;
+import instruments.Drum;
 import items.Item;
 
 import java.util.ArrayList;
@@ -10,17 +11,27 @@ public class Shop {
     Shop shop;
     Item item;
     DrumStick drumStick;
-    ArrayList<Item> stock;
+    private ArrayList<Item> stock;
     private String name;
 
     public Shop(Item item, DrumStick drumStick, String name) {
         this.shop = shop;
         this.item = item;
         this.drumStick = drumStick;
-        this.stock = stock;
+        this.stock = new ArrayList<>();
         this.name = name;
     }
 
+    public int getStockSize(){
+      return stock.size();
+    }
+
+    public void addItem (Item item){
+        this.stock.add(item);
+    }
 
 
+    public Item removeItem() {
+        return stock.remove(0);
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,9 +1,15 @@
 import accessories.DrumStick;
+import enum_package.InstrumentType;
+import instruments.Drum;
+import instruments.Piano;
 import items.Item;
 import org.junit.Before;
+import org.junit.Test;
 import shop.Shop;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShopTest {
 
@@ -16,7 +22,19 @@ public class ShopTest {
     @Before
     public void setUp() throws Exception {
         shop = new Shop(item, drumStick,"Guitar Guitar");
+        item = new Drum(1000.00, 3000.00, "Metal", "silver", InstrumentType.PERCUSSION);
     }
 
+    @Test
+    public void testAddItem() {
+        shop.addItem(item);
+        assertEquals(1, shop.getStockSize());
+    }
 
+    @Test
+    public void testRemoveItem() {
+        shop.addItem(item);
+        shop.removeItem();
+        assertEquals(0, shop.getStockSize());
+    }
 }
